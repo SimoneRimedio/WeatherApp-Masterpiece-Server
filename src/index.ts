5c3958abdb732b7cfbbe5fdcb70d4de22df1b4c3
SimoneRimedio <rimediosimone1@gmail.com>
2024-05-25 19:17:13 +0200

    cors added

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import router from './routes/routes';
 
 dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 3001;
 
-app.use("/",router)
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+app.use("/api",router)
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
