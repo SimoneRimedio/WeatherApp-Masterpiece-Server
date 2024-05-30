93033353f91b3a8736a28b913cd8840c000b5450
SimoneRimedio <rimediosimone1@gmail.com>
2024-05-30 16:36:42 +0200

    getGeocode controller deleted

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,8 @@
 import { Router } from "express";
-
-import getGeocode from "../controllers/getGeocode";
 import getReverseGeocode from "../controllers/getReverseGeocode";
 
 const router = Router();
 
-router.get("/getgeocode", getGeocode);
 router.get("/getreversegeocode", getReverseGeocode);
 
 export default router;
